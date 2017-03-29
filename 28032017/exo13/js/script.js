9b074d5edfb8034a6b686040955fc8ed33ef2cbe
Etudiant <korser02@gmail.com>
2017-03-29 09:32:39 +0200

    Javascript

--- 28032017/exo13/js/script.js
+++ 28032017/exo13/js/script.js
@@ -5,17 +5,12 @@ function CarType(paramDoors,paramColor,paramBrand,paramGear){
     this.color = paramColor;
     this.brand = paramBrand;
     this.gear = paramGear;
-    this.welcome = function(){
-        console.log('Bonjour, je suis une '+this.brand+', je posséde '+this.doors+' portes et '+this.gear+' vitesse et je suis de couleur '+this.color);
-    };
 };
 
 // Ajouter une fonction au type d'objet CarType
-/* CarType.prototype.welcome = function(){
-
+ CarType.prototype.welcome = function(){
     console.log('Bonjour, je suis une '+this.brand+', je posséde '+this.doors+' portes et '+this.gear+' vitesse et je suis de couleur '+this.color);
-
-};*/
+};
 
 // Créer une déclinaison de CarType
 
