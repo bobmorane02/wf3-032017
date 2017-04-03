868927c340fe5fb2293f7e2d87b74c6346dd0cfd
Etudiant <korser02@gmail.com>
2017-04-03 13:06:06 +0200

    Exercice Javascript

--- 03042017/js/script.js
+++ 03042017/js/script.js
@@ -4,20 +4,33 @@ $('document').ready(function(){
     // Vérifier le genre de l'avatarAge
     var avatarWoman = $('#avatarWoman');
     var avatarMan = $('#avatarMan');
+    var avatarGender;
 
     // => capter le click sur avatarWoman et avatarMan
     $(avatarWoman).click(function(){
-        console.log('je suis une '+avatarWoman.val());
         // désactiver avatarMan
         avatarMan.prop('checked',false);
+        // modifier la valeur de avatarGender
+        avatarGender = avatarWoman.val();
+        // Vider le message d'erreur
+        $('.labelGender b').text('');
     });
 
     $(avatarMan).click(function(){
-        console.log('je suis un '+avatarMan.val());
         // désactiver avatarWoman
         avatarWoman.prop('checked',false);
+        // modifier la valeur de avatarGender
+        avatarGender = avatarMan.val();
+        // Vider le message d'erreur
+        $('.labelGender b').text('');          
     });    
     
+    // Supprimer les messages d'erreur
+    $('input,select').focus(function(){
+        //Séléctionner la balise précédente
+        $(this).prev().children('b').text('');
+    });
+
     // Capter la soumission du formulaire
     $('form').submit(function(e){
 
@@ -29,35 +42,56 @@ $('document').ready(function(){
         var avatarAge = $('#avatarAge').val();
         var avatarStyleTop = $('#avatarStyleTop').val();
         var avatarStyleBottom = $('#avatarStyleBottom').val();
+        var formValid = 0;
         
         // Vérifier les champs du formulaire
             // => avatarName ninimum 5 caratéres
         if(avatarName.length < 5){
-            console.log('Min. 5 caractéres');
+            $('[for="avatarName"] b').text('Min. 5 caractéres');
         } else {
-            console.log('avatarName OK');
+            formValid++;
         };
 
-            // => avatarAge entre 1 et 100
-       if(avatarAge<1 || avatarAge >100 || avatarAge == ''){
-            console.log('avatarAge hors limites')
+                // => avatarAge entre 1 et 100
+        if(avatarAge<1 || avatarAge >100 || avatarAge == ''){
+            $('[for="avatarAge"] b').text('Hors limites');
         } else {
-            console.log('avatarAge OK');
+            formValid++;
         };
 
         // => avatarStyleTop obligatoire
         if(avatarStyleTop == 'null'){
-            console.log('Vous devez choisir un Haut');
+            $('[for="avatarStyleTop"] b').text('Vous devez choisir un Haut');
         } else {
-            console.log('Haut OK');
-        }
+            formValid++;
+        };
 
         // => avatarStyleBottom obligatoire
         if(avatarStyleBottom == 'null'){
-            console.log('Vous devez choisir un Bas');
+            $('[for="avatarStyleBottom"] b').text('Vous devez choisir un Bas');
         } else {
-            console.log('Bas OK');
-        }             
+            formValid++;
+        };
+
+        // => avatarGender vérifier la valeur
+        console.log(avatarGender);
+        if( avatarGender == undefined){
+            $('.labelGender b').text('Vous devez choisir un genre');
+        } else {
+            formValid++;
+        };
+
+        // Validation finale : vérifier formValid
+        console.log(formValid);
+        if (formValid == 5){
+            
+            // Envoyer les données au serveur et attendre la réponse du serveur.
+
+            // Vider les champs du formulaire
+
+            console.log('Formulaire OK');
+            $('form')[0].reset();
+        }; 
                   
     });
 
