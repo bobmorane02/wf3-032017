d851e9185dad8dc4565b53152b73f2969683336a
Etudiant <korser02@gmail.com>
2017-04-28 14:16:40 +0200

    Site e-commerce 2

--- PHP/08-Site/inc/fonction.inc.php
+++ PHP/08-Site/inc/fonction.inc.php
@@ -23,3 +23,28 @@ function internauteEstConnecteEtEstAdmin () {
         return false;
     }
 }
+
+function executeRequete ($req,$param = array()) {   // $param est un array vide par defaut : il est donc optionnel
+    // htmlspecialchars :
+    if (!empty($param)){
+        // Si on a bien reçu un array non vide on le traite
+        foreach ($param as $indice => $valeur) {
+            $param[$indice] = htmlspecialchars($valeur, ENT_QUOTES);    // transforme en entité HTML chaque caractéres
+        }                                                               // spécial, dont les quotes "'"
+    }
+
+    // La requête préparée :
+    global $pdo;    // On récupére la variable globale $pdo déclarée à l'extérieur de la function
+    $r = $pdo->prepare($req);
+    $succes = $r->execute($param);  // On execute la requête en lui passant l'array $param qui permet d'associer
+                                    // chaque marqueur à sa valeur
+
+    // Traitement erreur SQL événtuelle :
+    if (!$succes) { // si $succes vaut false, il y a une erreur sur la requête
+        die("Erreur sur la requête SQL : $r->errorinfo()[2]");  // die arrête le script et affiche le message d'erreur :  
+    }                                                           // On récupére l'erreur en clair se situant à l'indice 2
+                                                                // de l'array renvoyé par la méthode errorinfo() de l'objet
+                                                                // PDOStatement $r
+    return $r;  // retourne l'objet PDOStatement qui contient le résultat de la requête
+}                                   
+
--- PHP/08-Site/page_test.php
+++ PHP/08-Site/page_test.php
@@ -4,6 +4,6 @@ require_once('inc/init.inc.php');
 
 require_once('inc/haut.inc.php');
 
-echo 'Ceci est le contenu de la page';
+echo '<p>Ceci est le contenu de la page</p>';
 
 require_once('inc/bas.inc.php');
