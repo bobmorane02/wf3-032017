ae9110744758b8e558f3db25aeef244b7f926075
Etudiant <korser02@gmail.com>
2017-05-02 15:34:41 +0200

    site e-commerce

--- PHP/08-Site/admin/gestion_boutique.php
+++ PHP/08-Site/admin/gestion_boutique.php
@@ -8,13 +8,32 @@ if (!internauteEstConnecteEtEstAdmin()) {
     exit();                                 // de connexion qui est à la racine du site.
 }
 
+// 7- suppression d'un produit :
+if (isset($_GET['action']) && $_GET['action'] == 'supprimer' && isset($_GET['id_produit'])) {
+
+    // On sélectionne en BDD la photo pour pouvoir supprimer le fichier photo correspondant
+    $resultat = executeRequete("SELECT photo FROM produit WHERE id_produit = :id_produit",array(':id_produit'=>$_GET['id_produit']));
+
+    $produit_a_supprimer = $resultat->fetch(PDO::FETCH_ASSOC);  // pas de while car un seul id_produit
+    $chemin_photo_a_supprimer = $_SERVER['DOCUMENT_ROOT'].$produit_a_supprimer['photo']; // chemin du fichier à supprimer
+
+    if(!empty($produit_a_supprimer['photo']) && file_exists($chemin_photo_a_supprimer)) {
+        // Si il y a un chemin de photo en base ET que le fichier existe, on peut le supprimer :
+        unlink($chemin_photo_a_supprimer); // supprime le fichier spécifié
+    }
+    // Puis suppression du produit en BDD :
+    executeRequete("DELETE FROM produit WHERE id_produit = :id_produit",array(':id_produit'=>$_GET['id_produit']));
+
+    $contenu .= '<p class="bg-success">Le produit a été supprimé !</p>';
+    $_GET['action'] = 'affichage';  // On lance l'affichage des produits dans le tableaèu (étape 6)
+}
 // 4- Enregistrement du produit en BDD
 if ($_POST) { // équivalent à !empty($_POST) car si $_POST est rempli, il vaut true = formulaire posté
-    // Ici il faudrait contrôler tous les champs du formaulaire
+    // Ici il faudrait contrôler tous les champs du formulaire
 
     $photo_bdd = ''; // La photo subit un traitement spécifique en BDD. Cette variable contiendra son chemin d'accès.
 
-    // 5- Traitement de la photo :
+// 5- Traitement de la photo :
     if (!empty($_FILES['photo']['name'])) { // si une image a été uploadée, $_FILES est remplie
         // On constitue un nom unique pour le fichier photo :
         $nom_photo = $_POST['reference'].'_'.$_FILES['photo']['name'];
@@ -24,7 +43,7 @@ if ($_POST) { // équivalent à !empty($_POST) car si $_POST est rempli, il vaut
 
         // On constitue le chemin absolu complet de la photo depuis la racine serveur :
         $photo_dossier = $_SERVER['DOCUMENT_ROOT'].$photo_bdd;
-        
+
         // Enregistrement du fichier photo sur le serveur :
         copy($_FILES['photo']['tmp_name'],$photo_dossier);  // On copie le fichier temporaire de la photo indiqué par $_FILES['photo']['tmp_name']
                                                             // dans le chemin $photo_dossier de notre serveur.
@@ -45,7 +64,40 @@ $contenu .= '<ul class="nav nav-tabs">
                 <li><a href="?action=ajout">Ajout d\'un produit</a></li>
              </ul>';
 
+// 6- Affichage des produits dans le back-office :
+if (isset($_GET['action']) && $_GET['action'] == 'affichage' || !isset($_GET['action'])) {  // si $_GET contient 'affichage' ou que l'on arrive sur la page
+                                                                                            // pour la 1ere fois ($_GET['action'] n'existe pas)
+    $resultat = executeRequete("SELECT * FROM produit");    // On sélectionne tous les produits
+    $contenu .= '<h3>Affichage des produits</h3>';
+    $contenu .= '<p>Nombre de produit(s) dans la boutique : '.$resultat->rowCount().'</p>';
 
+    $contenu .= '<table class="table table-striped table-bordered">';
+        $contenu .= '<tr>';
+            for($i = 0;$i < $resultat->columnCount();$i++){
+                $colonne = $resultat->getColumnMeta($i);    // getColumnMeta() retourne un array contenant notamment le nom de la colonne
+                $contenu .= '<th>'.$colonne['name'].'</th>';
+            }
+            $contenu .= '<th>Action</th>'; // On ajoute une colonne "action"
+        $contenu .='</tr>';
+
+        // Affichage des lignes :
+        while ($ligne = $resultat->fetch(PDO::FETCH_ASSOC)){
+            $contenu .= '<tr>';
+                foreach($ligne as $index => $data){ // $index receptionne les indices, $data les valeurs
+                    if ($index == 'photo') {
+                        $contenu .= '<td><img src="'.$data.'" width="70" height ="70"></td>';
+                    } else {
+                        $contenu .= '<td>'.$data.'</td>';
+                    }
+                }
+                $contenu .='<td>
+                                <a href="?action=modification&id_produit='.$ligne['id_produit'].'">modifier</a>
+                                <a href="?action=supprimer&id_produit='.$ligne['id_produit'].'" onclick="return(confirm(\'Etes-vous certain de vouloir supprimer ce produit ?\'));">supprimer</a>
+                            </td>';
+            $contenu .= '</tr>';
+        }
+    $contenu .= '</table>';
+}
 
 
 // --------------------------------------------  AFFICHAGE --------------------------------------------
