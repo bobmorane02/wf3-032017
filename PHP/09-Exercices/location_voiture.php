16878699ec32fde0f772ff8d7d9d05f56231963f
Etudiant <korser02@gmail.com>
2017-05-09 11:20:38 +0200

    Exercices

---
+++ PHP/09-Exercices/location_voiture.php
@@ -0,0 +1,85 @@
+<?php
+/*
+   1- Vous créez un formulaire avec un menu déroulant avec les catégories A,B,C et D de véhicules de location et un champ nombre de jours de location. Lorsque le formulaire est soumis, vous affichez "La location de votre véhicule sera de X euros pour Y jour(s)." sous le formulaire.
+
+   2- Vous validez le formulaire : la catégorie doit être correcte et le nombre de jours un entier positif.
+
+   3- Vous créez une fonction prixLoc qui retourne le prix total de la location en fonction du prix de la catégorie choisie (A : 30€, B : 50€, C : 70€, D : 90€) multiplié par le nombre de jours de location. 
+
+   4- Si le prix de la location est supérieur à 150€, vous consentez une remise de 10%.
+
+*/
+
+// -------------------------------------------- Traitement ----------------------------------------------
+$message = '';
+
+function prixLoc ($nb_jours,$prix) {
+    return $nb_jours * $prix;
+}
+
+if (!empty($_POST)) {
+    $cat = array('A','B','C','D');
+    $jours = intval($_POST['nbj']);
+
+    if ( !in_array($_POST['categorie'],$cat)) {
+        $message .= nl2br("Catégorie invalide\n");
+    }
+
+    if ( $jours <= 0 ) {
+        $message .= 'Nombres de jours invalide';
+    }
+
+    if (empty($message)) {
+        switch ($_POST['categorie']) {
+            case 'A' : $total = prixLoc($jours,30);break;
+            case 'B' : $total = prixLoc($jours,50);break;
+            case 'C' : $total = prixLoc($jours,70);break;
+            default  : $total = prixLoc($jours,90);
+        }
+
+        if ($total > 150) {
+            $total = $total - ($total*10)/100;
+        }
+
+        $message .= "La location de votre véhicule sera de $total euros pour $jours jour(s).";
+    }
+
+} else {
+    $message .= 'Veuillez remplir le formulaire';
+}
+
+
+// -------------------------------------------- Affichage  ----------------------------------------------
+?>
+
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+    <h1>Location de véhicule</h1>
+    <form method="post" action="">
+        <p>
+            <label for="categorie">Catégorie de Véhicule</label>
+            <select name="categorie" id="categorie">
+                <option value="A">A</option>
+                <option value="B">B</option>
+                <option value="C">C</option>
+                <option value="D">D</option>
+            </select>
+        </p>
+        <p>
+            <label for="nbj">Nombre de jours</label>
+            <input type="text" id="nbj" name="nbj" value="0">
+        </p>
+        <p>
+            <input type="submit" value="Valider">
+        </p>
+    </form>
+    <div><?php echo $message; ?></div>
+</body>
+</html>
