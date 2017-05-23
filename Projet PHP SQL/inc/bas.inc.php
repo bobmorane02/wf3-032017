c77e6d0a814bbac734e6a6a55c9536a85863cad5
Unknown <korser02@gmail.com>
2017-05-24 01:12:43 +0200

    PHP SQL

---
+++ Projet PHP SQL/inc/bas.inc.php
@@ -0,0 +1,6 @@
+    <footer>
+        <div id="modale" class="hidden"></div>
+    </footer>
+    <script src="js/script.js"></script>
+</body>
+</html>
--- Projet PHP SQL/index.php
+++ Projet PHP SQL/index.php
@@ -1,45 +1,6 @@
 <?php
-    require_once ('inc/init.inc.php');
-
-        if (empty($_SESSION['mode']) || !in_array($_SESSION['mode'],array(0,1)) || session_status() != PHP_SESSION_ACTIVE){
-            $menu = 'json/menu_visiteur.json';
-        } elseif ($_SESSION['mode'] == 0) {
-            $menu = 'json/menu_membre.json';
-        } else {
-            $menu = 'json/menu_admin.json';
-        }
+include_once ('inc/haut.inc.php');
 ?>
-
-<!DOCTYPE html>
-<html lang="fr">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="stylesheet" href="css/style.css">
-    <title>Sallea</title>
-</head>
-<body>
-    <header>
-        <section>
-            <ul>
-                <li><a href="#"><h1>Sallea</h1></a></li>
-                <li><a href="#">Qui Sommes Nous</a></li>
-                <li><a href="#">Contact</a></li>
-                <li>
-                    Espace Membre
-                    <ul id="menu"><?php
-                            $fichier = file_get_contents($menu);
-                            $json = json_decode($fichier,true);
-                            foreach ($json as $valeur) {
-                                echo '<li id="'.strtolower($valeur).'">'.$valeur.'</li>';
-                            }
-                        ?>
-                    </ul>
-                </li>
-            </ul>
-        </section>
-    </header>
     <main>
         <section>
             <aside>
@@ -80,9 +41,6 @@
         </section>
         <section id="corp"></section>
     </main>
-    <footer>
-        <div id="modale"></div>
-    </footer>
-    <script src="js/script.js"></script>
-</body>
-</html>
+<?php
+include_once('inc/bas.inc.php');
+?>
