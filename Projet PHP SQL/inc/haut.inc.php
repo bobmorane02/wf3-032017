d640560bb9f8ebd9c8b9bb54f82b0a2937a716d4
Unknown <korser02@gmail.com>
2017-05-29 00:10:27 +0200

    PHP SQL

--- Projet PHP SQL/inc/haut.inc.php
+++ Projet PHP SQL/inc/haut.inc.php
@@ -1,5 +1,5 @@
 <?php
-    require_once ('inc/init.inc.php');
+    require_once ('init.inc.php');
 
         if (empty($_SESSION['mode']) || !in_array($_SESSION['mode'],array(0,1)) || session_status() != PHP_SESSION_ACTIVE){
             $menu = 'json/menu_visiteur.json';
@@ -16,29 +16,54 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet" integrity="sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN" crossorigin="anonymous">
+    <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">    
     <link rel="stylesheet" href="css/style.css">
     <title>Sallea</title>
 </head>
 <body>
     <header>
-        <section>
+        <div class="container">
+            <nav class="navbar navbar-default navbar-collapse">
+                <div class="navbar-header">
+                    <a class="navbar-brand" href="#">SalleA</a>
+                </div>
+                <ul class="nav navbar-nav">
+                    <li><a href="#">Qui sommes nous</a></li>
+                    <li><a href="#">Contact</a></li>
+                </ul>
+                <ul class="nav navbar-nav navbar-right">
+                    <li class="dropdown"><a href="#" class="dropdown-toggle" data-toggle="dropdown"><span class="glyphicon glyphicon-user"></span> Espace Membre</a>
+                        <ul class="dropdown-menu">
+                            <?php
+                                $fichier = file_get_contents($menu);
+                                $json = json_decode($fichier,true);
+                                foreach ($json as $valeur){
+                                    echo '<li><a href="#">'.$valeur.'</a></li>';
+                                }
+                            ?>
+                        </ul>  
+                    </li>
+                </ul>
+            </nav>
+        
+        <!--<section>
             <ul>
                 <li><a href="#"><h1>Sallea</h1></a></li>
                 <li><a href="#">Qui Sommes Nous</a></li>
                 <li><a href="#">Contact</a></li>
                 <li>
                     Espace Membre
-                    <ul id="menu"><?php
+                    <ul id="menu"><?php/*
                             $fichier = file_get_contents($menu);
                             $json = json_decode($fichier,true);
                             foreach ($json as $index => $valeur) {
                                 echo '<li id="'.$index.'">'.$valeur.'</li>';
                             }
-                        ?>
+                        */?>
                     </ul>
                 </li>
             </ul>
-        </section>
+        </section>-->
     </header>
     <main>
--- Projet PHP SQL/index_1.php
+++ Projet PHP SQL/index_1.php
@@ -1,45 +1,47 @@
 <?php
-include_once ('inc/start.inc.php');
 include_once ('inc/haut.inc.php');
 ?>
-        <section>
-            <aside>
-                <div>
-                    <h4>Catégorie</h4>
-                    <button>Reunion</button>
-                    <button>Bureau</button>
-                    <button>Formation</button>
+        <div class="col-md-3">
+            <div class="btn-group-vertical" role="group">
+                <h5 class="text-left"><strong>Catégorie</strong></h5>
+                <button type="button" class="btn btn-default">Réunion</button>
+                <button type="button" class="btn btn-default">Bureau</button>
+                <button type="button" class="btn btn-default">Formation</button>
+            </div>
+            <div class="btn-group-vertical" role="group">
+                <h5 class="text-left"><strong>Ville</strong></h5>
+                <button type="button" class="btn btn-default">Paris</button>
+                <button type="button" class="btn btn-default">Lyon</button>
+                <button type="button" class="btn btn-default">Marseille</button>
+            </div>
+            <div class="form-group">
+                <label for="capacite"><strong>Capacité</strong></label>
+                <select class="form-control" name="capacite" id="capacite">
+                    <option value="3">3</option>
+                    <option value="5">5</option>
+                    <option value="10">10</option>
+                    <option value="20">20</option>
+                    <option value="50">50</option>
+                    <option value="100">100</option>
+                </select>
+            </div>
+            <div class="form-group">
+                <label for="prix"><strong>Prix</strong></label>                
+                <input type="range" min="0" max="3000" step="50" name="prix" id="prix">
+                <p class="text-left"><em>maximum <span id="max"></span>1000</em> €</p>
+            </div>
+            <div class="form-group">
+                <p class="text-left"><strong>Période</strong></p>                
+                <div class="text-left"><em>Date d'arrivée</em></div>
+                <div class="input-group">                        
+                    <span class="input-group-addon">@</span>
+                    <input type="text" class="form-control" name="date_arrivee">
                 </div>
-                <div>
-                    <h4>Ville</h4>
-                    <button>Paris</button>
-                    <button>Lyon</button>
-                    <button>Marseille</button>
-                </div>
-                <div>
-                    <h4>Capacité</h4>
-                    <select name="capacite" id="capatite">
-                        <option value="3">3</option>
-                        <option value="5">5</option>
-                        <option value="10">10</option>
-                        <option value="20">20</option>
-                        <option value="50">50</option>
-                        <option value="100">100</option>
-                    </select>
-                </div>
-                <div>
-                    <h4>Prix</h4>
-                </div>
-                <div>
-                    <h4>Période</h4>
-                    <label for="arrivee">Date d'arrivée</label>
-                    <input id="arrivee" type="date" name="arrivee">
-                    <label for="depart">Date d'arrivée</label>
-                    <input id="depart" type="date" name="depart">
-                </div>                   
-            </aside>
-        </section>
-        <section id="corp"></section>
+            </div>
+        </div>        
+    </div> <!-- Fin du container-->
+
+
 <?php
 include_once('inc/bas.inc.php');
 ?>
