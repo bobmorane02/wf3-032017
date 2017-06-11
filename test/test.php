d46e4227199b1fed01cc2ad5e8b0e54f94440774
Unknown <korser02@gmail.com>
2017-06-11 19:08:08 +0200

    Test

---
+++ test/test.php
@@ -0,0 +1,29 @@
+<?php
+
+set_time_limit(0);
+
+$db = new PDO('mysql:host=localhost;dbname=temp','root','',array(PDO::ATTR_ERRMODE => PDO::ERRMODE_WARNING,PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES utf8'));
+$geocoder = "http://maps.googleapis.com/maps/api/geocode/json?address=%s&sensor=false";
+
+$rep = $db->query("SELECT * FROM ville WHERE id_ville BETWEEN 2485 AND 2500");
+
+
+while ($reponse = $rep->fetch(PDO::FETCH_ASSOC)){
+    if (!$reponse['longitude'] || !$reponse['latitude'] || !$reponse['departement']){
+        $address = $reponse['code_postal'].' '.$reponse['nom_ville'];
+        $url_address = urlencode($address);
+        $query = sprintf($geocoder,$url_address);
+        $results = file_get_contents($query);
+        $resultat = json_decode($results);
+        $coord = $resultat->results[0]->geometry->location;
+        for ($i=0;$i < sizeof($resultat->results[0]->address_components);$i++) {
+            if($resultat->results[0]->address_components[$i]->types[0] == 'administrative_area_level_2'){
+                $departement = $resultat->results[0]->address_components[$i]->long_name;
+                break;
+            }
+        }
+        $maj = $db->query("UPDATE ville SET longitude = ".$coord->lng.",latitude = ".$coord->lat.", departement ='".$departement."' WHERE id_ville =".$reponse['id_ville']);
+    }
+#    $url_address = utf8_encode($address);
+#    echo '<pre>';print_r($resultat);echo '</pre>';
+}
