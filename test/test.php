fafc68acd90a24bcbdbd9ed28f062bc92b6e45e7
Unknown <korser02@gmail.com>
2017-06-12 22:31:57 +0200

    villes

--- test/test.php
+++ test/test.php
@@ -2,12 +2,11 @@
 
 set_time_limit(0);
 
-$db = new PDO('mysql:host=localhost;dbname=test','root','',array(PDO::ATTR_ERRMODE => PDO::ERRMODE_WARNING,PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES utf8'));
+$db = new PDO('mysql:host=localhost;dbname=temp','root','',array(PDO::ATTR_ERRMODE => PDO::ERRMODE_WARNING,PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES utf8'));
 $geocoder = "http://maps.googleapis.com/maps/api/geocode/json?address=%s&sensor=false";
 
-#$rep = $db->query("SELECT * FROM ville WHERE id_ville = 4724 AND 4700");
-$rep = $db->query("SELECT * FROM ville WHERE code_postal LIKE '14%' AND longitude IS NULL");
-
+$rep = $db->query("SELECT * FROM ville WHERE id_ville BETWEEN 6501 AND 7000 AND departement IS NULL");
+#$rep = $db->query("SELECT * FROM ville WHERE code_postal LIKE '14%' AND longitude IS NULL");
 
 while ($reponse = $rep->fetch(PDO::FETCH_ASSOC)){
     if (!$reponse['longitude'] || !$reponse['latitude'] || !$reponse['departement']){
@@ -26,5 +25,6 @@ while ($reponse = $rep->fetch(PDO::FETCH_ASSOC)){
         $maj = $db->query("UPDATE ville SET longitude = ".$coord->lng.",latitude = ".$coord->lat.", departement ='".$departement."' WHERE id_ville =".$reponse['id_ville']);
     }
 }
-
+/*$a = $rep->fetchAll(PDO::FETCH_ASSOC);
+echo '<pre>';print_r($a);echo '</pre>';*/
 echo "FINI !!!";
